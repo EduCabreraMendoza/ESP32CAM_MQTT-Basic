361b38f2eb38313613e0ca299617e7f78b3b6b6e
EduCabreraMendoza <edu.cabrera.mendoza@gmail.com>
2022-08-11 12:25:38 -0500

    Cambio en los temas de mosquitto

--- ESP32CAM_MQTT-Basic/ESP32CAM_MQTT-Basic.ino
+++ ESP32CAM_MQTT-Basic/ESP32CAM_MQTT-Basic.ino
@@ -98,7 +98,7 @@ void loop() {
     dtostrf(data, 1, 2, dataString);  // Esta es una función nativa de leguaje AVR que convierte un arreglo de caracteres en una variable String
     Serial.print("Contador: "); // Se imprime en monitor solo para poder visualizar que el evento sucede
     Serial.println(dataString);
-    client.publish("codigoIoT/ejemplo/MQTT", dataString); // Esta es la función que envía los datos por MQTT, especifica el tema y el valor
+    client.publish("codigoIoT/ejemplo/mqtt", dataString); // Esta es la función que envía los datos por MQTT, especifica el tema y el valor
   }// fin del if (timeNow - timeLast > wait)
 }// fin del void loop ()
 
@@ -126,17 +126,17 @@ void callback(char* topic, byte* message, unsigned int length) {
   // En esta parte puedes agregar las funciones que requieras para actuar segun lo necesites al recibir un mensaje MQTT
 
   // Ejemplo, en caso de recibir el mensaje true - false, se cambiará el estado del led soldado en la placa.
-  // El ESP323CAM está suscrito al tema codigoIoT/ejemplo/MQTTIN
-  if (String(topic) == "codigoIoT/ejemplo/MQTTIN") {  // En caso de recibirse mensaje en el tema codigoIoT/ejemplo/MQTTIN
+  // El ESP323CAM está suscrito al tema codigoIoT/ejemplo/mqttin
+  if (String(topic) == "codigoIoT/ejemplo/mqttin") {  // En caso de recibirse mensaje en el tema codigoIoT/ejemplo/mqttin
     if(messageTemp == "true"){
       Serial.println("Led encendido");
       digitalWrite(flashLedPin, HIGH);
-    }// fin del if (String(topic) == "codigoIoT/ejemplo/MQTTIN")
+    }// fin del if (String(topic) == "codigoIoT/ejemplo/mqttin")
     else if(messageTemp == "false"){
       Serial.println("Led apagado");
       digitalWrite(flashLedPin, LOW);
     }// fin del else if(messageTemp == "false")
-  }// fin del if (String(topic) == "codigoIoT/ejemplo/MQTTIN")
+  }// fin del if (String(topic) == "codigoIoT/ejemplo/mqttin")
 }// fin del void callback
 
 // Función para reconectarse
@@ -147,7 +147,7 @@ void reconnect() {
     // Intentar reconexión
     if (client.connect("ESP32CAMClient")) { //Pregunta por el resultado del intento de conexión
       Serial.println("Conectado");
-      client.subscribe("codigoIoT/ejemplo/MQTTIN"); // Esta función realiza la suscripción al tema
+      client.subscribe("codigoIoT/ejemplo/mqttin"); // Esta función realiza la suscripción al tema
     }// fin del  if (client.connect("ESP32CAMClient"))
     else {  //en caso de que la conexión no se logre
       Serial.print("Conexion fallida, Error rc=");
